1ad60eca877a8336df758020919aea48df08ae86
demirbileku <udemirbilek2@gmail.com>
2021-07-24 16:04:47 +0300

    commit tree 4e320d850b5109b125c918f1e111d8a15d9e76fd

--- src/features/astronauts/astronautsSlice.js
+++ src/features/astronauts/astronautsSlice.js
@@ -1,24 +1,38 @@
 // Action Creators
-export function fetchAstronauts(astronauts) {
-  return {
-    type: "astronauts/astronautsLoaded",
-    payload: astronauts,
+export function fetchAstronauts() {
+  return function (dispatch) {
+    dispatch({ type: "astronauts/astronautsLoading" });
+    fetch("http://api.open-notify.org/astros.json")
+      .then((response) => response.json())
+      .then((astronauts) =>
+        dispatch({
+          type: "astronauts/astronautsLoaded",
+          payload: astronauts.people,
+        })
+      );
   };
 }
 
 // Reducers
 const initialState = {
   entities: [], //array of astronauts
+  status: "idle", //loading status for fetch
 };
+
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case "astronauts/astronautsLoaded":
       return {
         ...state,
+        status: "idle",
         entities: action.payload,
       };
-
+    case "astronauts/astronautsLoading":
+      return {
+        ...state,
+        status: "loading",
+      };
     default:
       return state;
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
 import rootReducer from "./reducer";
 
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composedEnhancer
 );
 
 ReactDOM.render(
